ad271af1c64287b0e54b43959e3f41453043eb17
Andre Sencioles <asenci@gmail.com>
2021-04-08 15:51:04 +1200

    Fix state variable name

--- src/save.ts
+++ src/save.ts
@@ -8,7 +8,7 @@ function run(): void {
     const savePath = utils.getInputAsString('save-path', {required: true})
     const forceSave = utils.getInputAsBool('force-save')
 
-    const cacheHit = core.getState('CACHE_HIT')
+    const cacheHit = core.getState('CACHE_HIT_PRIMARY')
 
     if (!forceSave) {
       if (cacheHit.toUpperCase() === 'TRUE') {
